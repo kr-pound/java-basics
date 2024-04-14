13fe151c8d1e4240d6a1d2d8dd6ce2826ee9dbe9
kr-pound <kritapas.chuti@gmail.com>
2024-04-14 14:36:50 +0700

    update unused declaration

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
